591e469e340cf04f4f4c9529a63904fcac8104ba
Luis Martinez <luismartinezmilla@gmail.com>
2025-02-07 16:24:53 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Luis Martinez on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
